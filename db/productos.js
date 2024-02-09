a660390c10bff5cbd379280af4677f8ca424c785
Carolina buruzangaray <carolaburu@hotmail.com>
2024-02-08 22:28:45 -0300

    archivo modificado

--- db/productos.js
+++ db/productos.js
@@ -1,42 +1,34 @@
 export const productos = [
-    {
-      id: 1,
-      nombre: "Fideos Italiano",
-      precio: 1500,
-      imagen:"https://http2.mlstatic.com/D_NQ_NP_853111-MLA71484726223_092023-O.webp",
-      categoria:"fideos"
-    },
-    {
-      id: 2,
-      nombre: "Sal Marina fina",
-      precio: 800,
-      imagen:"https://neufood.com.ar/wp-content/uploads/2020/08/sal-fina-marina-macrozen1-7ca8f1481615ad146715780938387392-640-0.jpg",
-      categoria:"sal"
-    },
-    {
-      id: 3,
-      nombre: "Sal Marina gruesa",
-      precio: 1200,
-      imagen:"https://http2.mlstatic.com/D_NQ_NP_927661-MLA42075270930_062020-O.webp",
-      categoria:"sal"
-    },
-    {
-      id: 4,
-      nombre: "Fideos Arabe",
-      precio: 2000,
-      imagen:"https://cloudinary.images-iherb.com/image/upload/f_auto,q_auto:eco/images/tkt/tkt01074/l/8.jpg",
-      categoria:"fideos"
-    },
-    {
-      id: 5,
-      nombre: "Especias",
-      precio: 4500,
-      imagen:"https://m.media-amazon.com/images/I/81wL-bQ0jsL._AC_UF894,1000_QL80_.jpg",
-      categoria:"especias"
-    },
-    
-  ];
+  {
+    id: 1,
+    nombre: "Remera",
+    precio: 13000,
+    imagen:"https://www.pymedia.com.ar/wp-content/uploads/2017/06/remeras-pymedia2-600x600.jpg",
+    categoria:"remera"
+  },
+  {
+    id: 2,
+    nombre: "Jean",
+    precio: 54000,
+    imagen:"https://http2.mlstatic.com/D_NQ_NP_997699-MLA49888773208_052022-O.webp",
+    categoria:"jean"
+  },
+  {
+    id: 3,
+    nombre: "Campera",
+    precio: 155000,
+    imagen:"https://www.cordonandino.com/img/articulos/campera_inken_2_de_mujer_con_pluma_allied_800_fp_imagen1.jpg",
+    categoria:"campera"
+  },
+  {
+    id: 4,
+    nombre: "Gorra",
+    precio: 25000,
+    imagen:"https://acdn.mitiendanube.com/stores/987/679/products/jahtbher-r_21-b228a4a71285712c5b16934921269229-640-0.jpg",
+    categoria:"gorra"
+  },
   
-  
-  JSON.parse(localStorage.getItem("productos")) || localStorage.setItem("productos", JSON.stringify(productos));
-  
+];
+
+
+JSON.parse(localStorage.getItem("productos")) || localStorage.setItem("productos", JSON.stringify(productos));
