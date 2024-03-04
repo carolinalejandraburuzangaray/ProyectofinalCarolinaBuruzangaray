88fc25ac9c68171d1af96714e553953358eb88ef
Carolina buruzangaray <carolaburu@hotmail.com>
2024-03-04 11:11:52 -0300

    archivo modificado

--- db/productos.js
+++ db/productos.js
@@ -1,34 +1,19 @@
-export const productos = [
-  {
-    id: 1,
-    nombre: "Remera",
-    precio: 13000,
-    imagen:"https://www.pymedia.com.ar/wp-content/uploads/2017/06/remeras-pymedia2-600x600.jpg",
-    categoria:"remera"
-  },
-  {
-    id: 2,
-    nombre: "Jean",
-    precio: 54000,
-    imagen:"https://http2.mlstatic.com/D_NQ_NP_997699-MLA49888773208_052022-O.webp",
-    categoria:"jean"
-  },
-  {
-    id: 3,
-    nombre: "Campera",
-    precio: 155000,
-    imagen:"https://www.cordonandino.com/img/articulos/campera_inken_2_de_mujer_con_pluma_allied_800_fp_imagen1.jpg",
-    categoria:"campera"
-  },
-  {
-    id: 4,
-    nombre: "Gorra",
-    precio: 25000,
-    imagen:"https://acdn.mitiendanube.com/stores/987/679/products/jahtbher-r_21-b228a4a71285712c5b16934921269229-640-0.jpg",
-    categoria:"gorra"
-  },
-  
-];
 
+let listado = document.getElementById("listado");
 
-JSON.parse(localStorage.getItem("productos")) || localStorage.setItem("productos", JSON.stringify(productos));
+fetch("./data.json")
+then((response) => response.json())
+then((response) => {
+response.forEach((producto) => {
+const li = document.createElement("li");
+li.innerHTML = `
+<h2>Id: ${producto.id}</h2>
+<p>Nombre: ${producto.nombre}</p>
+<b>$${producto.precio}</b>
+     `;
+
+listado.append(li);
+});
+});
+
+//JSON.parse(localStorage.getItem("productos")) || localStorage.setItem("productos", JSON.stringify(productos));
